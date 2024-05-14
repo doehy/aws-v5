71e0b4acbd400e052813ae70928e7d13e33783c5
GKYoo <dbehxks0245@gmail.com>
2024-05-14 23:21:10 +0900

    feat : anyRequest security test

--- src/main/java/site/metacoding/awsv5/security/SecurityConfig.java
+++ src/main/java/site/metacoding/awsv5/security/SecurityConfig.java
@@ -12,12 +12,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http.authorizeRequests()
-                .antMatchers("/favicon.ico").permitAll()
-                .antMatchers("/actuator/**").permitAll() // 헬스체크 요청 경로에 대한 접근을 허용합니다.
-                .anyRequest().authenticated() // 다른 요청에 대해서는 인증이 필요합니다.
+                .antMatchers("/members/**").access("hasRole('ADMIN') or hasRole('MEMBER')")
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .antMatchers("/static/css/**").permitAll()
+                .antMatchers("/static/js/**").permitAll()
+                .antMatchers("/static/file/**").permitAll()
+                .anyRequest().permitAll()
                 .and()
-                .httpBasic(); // 기본 인증을 활성화합니다. (선택적)
+                .formLogin()
+                .loginPage("/require-login")
+                .loginProcessingUrl("/login")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling().accessDeniedPage("/fail-authorize");
+
         return http.build();
     }
 }
