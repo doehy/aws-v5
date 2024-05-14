d6cf20984de78759d16b5a16dff50456dad85da2
GKYoo <dbehxks0245@gmail.com>
2024-05-14 11:05:39 +0900

    feat : fourth test

--- src/test/java/site/metacoding/awsv5/AwsV5ApplicationTests.java
+++ src/test/java/site/metacoding/awsv5/AwsV5ApplicationTests.java
@@ -1,8 +1,11 @@
 package site.metacoding.awsv5;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AwsV5ApplicationTests {
-
+    @Test
+    void contextLoads() {
+    }
 }
